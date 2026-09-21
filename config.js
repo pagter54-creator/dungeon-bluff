5c0002ce5b89e5b7d8411a4b75a168935809b529
pagter54-creator <pagter54@gmail.com>
2026-09-22 04:40:24 +0900

    Add files via upload

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // 브라우저에는 publishable key 또는 legacy anon key만 넣으세요.
-export const SUPABASE_URL = 'https://lktjmuhbfnahdghidqfa.supabase.co';
-export const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_8IsfpRB3wO-KcJ_B4WEN_w_Ax8dhCkb';
+export const SUPABASE_URL = 'https://dvpisuvyeyqwhewfbnls.supabase.co';
+export const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_iLo2PO2ZYEwMQhjqtFR3NA_QKag65tr';
